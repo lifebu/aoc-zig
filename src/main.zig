4187cffe60d59a1b95ceff58203d25ac9b2b32dd
cburmeister <cburmeister@MNZ-WKS-10858>
2024-12-05 22:00:42 +0100

    Test comit wks

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,7 @@ pub fn main() !void {
     const stdout = bw.writer();
 
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    std.debug.print("Hello two");
 
     try bw.flush(); // don't forget to flush!
 }
